32664a113f0cde9bdb15b259e76f8d99ce94ab9f
Shixin Zeng <zeng.shixin@gmail.com>
2010-03-09 20:17:12 -0600

    init.sh changes from OAH to scons

--- init.sh
+++ init.sh
@@ -1,37 +1,49 @@
 #!/bin/sh
-GNOME_MODULES="gtk pango atk glib libxml2 totem-plparser totem"
-CAIRO_MODULES="cairo pixman"
-OTHER_MODULES="png"
-ALL_MODULES="$GNOME_MODULES $CAIRO_MODULES $OTHER_MODULES"
+GNOME_MODULES="gtk pango atk glib libxml2 librsvg gmime totem-plparser totem evince"
+FREEDESKTOP_MODULES="cairo pixman poppler"
+MODULES_WITH_UPSTREAM=("png")
+MODULES_UPSTREAM=("git://libpng.git.sourceforge.net/gitroot/libpng/libpng")
+OTHER_MODULES="freetype"
+ALL_MODULES="$GNOME_MODULES $FREEDESKTOP_MODULES $MODULES_WITH_UPSTREAM $OTHER_MODULES"
 for mod in $ALL_MODULES
 do 
+	git clone git@github.com:zsx/$mod.git
 	cd $mod
 	echo "rename $mod's origin to github" 
 	git remote rename origin github
-	git branch OAH --track github/OAH
-	git checkout OAH
+	git branch scons --track github/scons
+	git checkout scons
 	cd ..
 done
 
+add_origin()
+{
+#add_origin mod origin
+	cd $1
+	echo "adding $1's origin"
+	echo $2
+	git remote add origin $2
+   	cd ..
+}
+
+add_origin_root() 
+{
+#add_origin_root mod root
+	add_origin $1 $2/$1
+}
 for mod in $GNOME_MODULES
 do 
-	cd $mod
-	echo "adding $mod's origin"
-	git remote add origin git://git.gnome.org/$mod
-   	cd ..
+	add_origin_root $mod git://git.gnome.org 
 done
 
-for mod in $CAIRO_MODULES
+for mod in $FREEDESKTOP_MODULES
 do 
-	cd $mod
-	echo "adding $mod's origin"
-	git remote add origin git://anongit.freedesktop.org/git/$mod
-   	cd ..
+	add_origin_root $mode git://anongit.freedesktop.org/git
 done
 
-cd png
-echo "adding png's origin"
-git remote add origin git://libpng.git.sourceforge.net/gitroot/libpng/libpng
-cd ..
+for ((i = 0; i < ${#MODULES_WITH_UPSTREAM[@]}; i++))
+do
+	add_origin ${MODULES_WITH_UPSTREAM[$i]} ${MODULES_UPSTREAM[$i]}
+done
 
-git submodule init
+#git submodule init
