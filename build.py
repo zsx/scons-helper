15e7bbde1a1819ca336dc5d1f5b1b5ecc03d61ea
Shixin Zeng <zeng.shixin@gmail.com>
2010-03-13 18:40:38 -0600

    abort upon failure

--- build.py
+++ build.py
@@ -5,11 +5,12 @@ import subprocess
 import os
 import sys
 
-top_dir = os.path.dirname(__file__)
+top_dir = os.path.abspath(os.path.dirname(__file__))
 
 packs = ['zlib',
          'pixman',
          'png',
+         'jpeg-7',
          'freetype2',
          'fontconfig',
          'cairo',
@@ -17,14 +18,15 @@ packs = ['zlib',
          'libxml2',
          'atk', 
          'pango',
-         'librsvg',
-         'gtk']
-
-args = [r'--site-dir=..', r'PREFIX=C:\FOSS\Debug', 'DEBUG=1', r'PERL=C:\Perl\bin\perl.exe', 'build_test=1'] + sys.argv[1:]
+         #'librsvg', waiting for pangoft2
+         'gtk',
+         'poppler',
+         'evince']
 
 def build():
     log = file('build.log', 'w')
     for p in packs:
+        args = [r'--site-dir=..', r'PREFIX=C:\FOSS\Debug', 'DEBUG=1', r'PERL=C:\Perl\bin\perl.exe', 'build_test=1'] + sys.argv[1:]
         os.chdir(p)
         print "Building ", p,
         ret = subprocess.Popen(['scons'] + args, shell=True, stdout = log).wait()
@@ -32,13 +34,19 @@ def build():
             print ": Success"
         else:
             print ": Failure"
+            log.close()
+            return
 
+        if p in ['pango', 'glib']:
+            args += [r'--install-sandbox=C:\FOSS\debug']
         print "Installing ", p,
         ret = subprocess.Popen(['scons'] + args + ['install'], shell=True, stdout = log).wait()
         if not ret:
             print ": Success"
         else:
             print ": Failure"
+            log.close()
+            return
         os.chdir(top_dir)
     log.close()
 
